70d7620b10d3a4ceab85e097481f545c31463e66
Nicola Hearn <nicolahearn@mastude5h3vq6l4.home>
2022-11-02 12:48:36 +0000

    adds 5 tests that pass

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,10 +1,14 @@
 
 def fizzbuzz (number)
-  if number % 3 == 0 && number % 5 == 0
+  if !number.is_a? Integer
+    "Please input number"
+  elsif number % 3 == 0 && number % 5 == 0
         "fizzbuzz"
   elsif number % 3 == 0
         "fizz"
   elsif number % 5 ==0
         "buzz"
+  else 
+    number 
   end 
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,6 +1,10 @@
 require 'fizzbuzz'
 
 describe 'fizzbuzz' do
+
+  it 'returns "Please input number" when given anything other than an integer' do
+    expect(fizzbuzz("hello")).to eq 'Please input number'
+  end 
   it 'returns "fizzbuzz" when passed a multiple of 5 and 3' do
     expect(fizzbuzz(15)).to eq 'fizzbuzz'
   end
@@ -9,5 +13,8 @@ describe 'fizzbuzz' do
   end
   it 'returns "buzz" when passed a multiple of 5' do
     expect(fizzbuzz(5)).to eq 'buzz'
+  end
+  it 'returns the number when the number is not a multiple of 3 or 5' do
+    expect(fizzbuzz(8)).to eq 8
   end
 end
