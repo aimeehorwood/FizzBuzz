45bbe96bebee96f1da40e0a5b0c13fd4b25f4d23
Nicola Hearn <nicolahearn@mastude5h3vq6l4.home>
2022-11-02 12:41:13 +0000

    adds fizz, buzz and fizzbuzz tests

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,13 @@
 require 'fizzbuzz'
 
 describe 'fizzbuzz' do
+  it 'returns "fizzbuzz" when passed a multiple of 5 and 3' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
   it 'returns "fizz" when passed a multiple of 3' do
     expect(fizzbuzz(9)).to eq 'fizz'
+  end
+  it 'returns "buzz" when passed a multiple of 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
   end
 end
